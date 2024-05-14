ac99512fef9ae92d6f541ffd760ce2b795915b31
Tony Solomonik <tony.solomonik@gmail.com>
2024-05-14 16:55:54 +0300

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use opendal::{
     layers::{BlockingLayer, LoggingLayer},
     BlockingOperator, Operator,
 };
+use pretty_env_logger::formatted_timed_builder;
 use tantivy::{
     collector::TopDocs,
     directory::{FileSlice, MmapDirectory},
@@ -34,6 +35,13 @@ use crate::{
     opendal_file_handle::OpenDalFileHandle,
     unified_index::writer::UnifiedIndexWriter,
 };
+
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+const DEFAULT_DEBUG_LOG_LEVEL: &str = "toshokan=trace";
+const DEFAULT_RELEASE_LOG_LEVEL: &str = "toshokan=info";
 
 static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
     tokio::runtime::Builder::new_multi_thread()
@@ -86,20 +94,20 @@ fn index(args: IndexArgs) -> Result<()> {
 
         i += 1;
         if i % 1000 == 0 {
-            println!("{i}");
+            debug!("{i}");
         }
     }
 
-    println!("Commiting {added} documents, after processing {i}");
+    info!("Commiting {added} documents, after processing {i}");
     index_writer.commit()?;
 
     if args.merge {
         let segment_ids = index.searchable_segment_ids()?;
-        println!("Merging {} segments", segment_ids.len());
+        info!("Merging {} segments", segment_ids.len());
         index_writer.merge(&segment_ids).wait()?;
     }
 
-    println!("Joining merging threads");
+    info!("Joining merging threads");
     index_writer.wait_merging_threads()?;
 
     let unified_index_writer = UnifiedIndexWriter::from_file_paths(
@@ -125,7 +133,7 @@ fn index(args: IndexArgs) -> Result<()> {
         .call()?
         .into_std_write();
 
-    println!("Writing unified index file");
+    info!("Writing unified index file");
     let (total_len, footer_len) = unified_index_writer.write(&mut writer)?;
     writer.close()?;
 
@@ -134,7 +142,7 @@ fn index(args: IndexArgs) -> Result<()> {
         footer_len.to_string(),
     )?;
 
-    println!(
+    debug!(
         "Index file length: {}. Footer length: {}",
         total_len, footer_len
     );
@@ -217,6 +225,18 @@ fn search(args: SearchArgs) -> Result<()> {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let default_log_level = if cfg!(debug_assertions) {
+        DEFAULT_DEBUG_LOG_LEVEL
+    } else {
+        DEFAULT_RELEASE_LOG_LEVEL
+    };
+
+    let mut log_builder = formatted_timed_builder();
+    log_builder.parse_filters(
+        &std::env::var("RUST_LOG").unwrap_or_else(|_| default_log_level.to_string()),
+    );
+    log_builder.try_init()?;
 
     let args = parse_args();
     match args.subcmd {
