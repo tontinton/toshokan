4a2a4819ce5cf7643be9f5514129ad802b885f37
Tony Solomonik <tony.solomonik@gmail.com>
2024-05-31 17:44:02 +0300

    metadata: Add `indexes` table
    This table holds the base path of all its index files + its
    configuration as `jsonb`.

---
+++ migrations/0001_indexes.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS indexes(
+    name TEXT PRIMARY KEY,
+    config jsonb NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS index_files(
+    id VARCHAR(36) PRIMARY KEY,
+    index_name TEXT NOT NULL REFERENCES indexes(name) ON DELETE CASCADE,
+    file_name TEXT NOT NULL,
+    footer_len BIGINT NOT NULL
+);
