e776f611f9e863a27c0d5a438354d3f5dc54ff8b
Tony Solomonik <tony.solomonik@gmail.com>
2024-06-11 20:22:19 +0300

    Add `lib.rs` to be able to import in integration tests

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod args;
+pub mod bincode;
+pub mod commands;
+pub mod config;
+pub mod merge_directory;
+pub mod opendal_file_handle;
+pub mod unified_index;
+
+#[macro_use]
+extern crate log;
