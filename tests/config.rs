8d7c79cb8fc34a67e058a08cf066ae127728acf8
Tony Solomonik <tony.solomonik@gmail.com>
2024-06-12 13:32:11 +0300

    tests: Add test for config with array of u64

--- tests/config.rs
+++ tests/config.rs
@@ -6,6 +6,8 @@ use clap::Parser;
 use color_eyre::Result;
 use ctor::ctor;
 use pretty_env_logger::formatted_timed_builder;
+use rstest::rstest;
+use tempfile::TempDir;
 use tokio::sync::mpsc;
 use toshokan::{
     args::{DropArgs, IndexArgs, SearchArgs},
@@ -27,18 +29,51 @@ fn init() {
     log_builder.try_init().unwrap();
 }
 
+#[rstest]
+#[case(
+    include_str!("../example_config.yaml"),
+    "hdfs-logs-multitenants-2.json",
+    "tenant_id:>50 AND severity_text:INFO",
+    r#"{"attributes":{"class":"org.apache.hadoop.hdfs.server.datanode.DataNode"},"body":"PacketResponder: BP-108841162-10.10.34.11-1440074360971:blk_1074072698_331874, type=HAS_DOWNSTREAM_IN_PIPELINE terminating","resource":{"service":"datanode/01"},"severity_text":"INFO","tenant_id":58,"timestamp":"2016-04-13T06:46:53Z"}"#,
+)]
+#[case(
+    "
+name: array_test
+path: /tmp/toshokan_array
+schema:
+  fields:
+    - name: array
+      array: true
+      type: !number
+        type: u64
+",
+    "array.json",
+    "*",
+    r#"{"array":[1,2,3,4]}"#,
+)]
+#[trace]
 #[tokio::test]
-async fn test_example_config() -> Result<()> {
+async fn test_config(
+    #[case] raw_config: &str,
+    #[case] index_test_file: &str,
+    #[case] query: &str,
+    #[case] expected_output: &str,
+) -> Result<()> {
     let postgres = run_postgres().await?;
-    let config = IndexConfig::from_str(include_str!("../example_config.yaml"))?;
+    let config = IndexConfig::from_str(raw_config)?;
 
     run_create_from_config(&config, &postgres.pool).await?;
+
+    let temp_dir = TempDir::new()?;
+    let temp_path = temp_dir.path().to_string_lossy();
 
     run_index(
         IndexArgs::parse_from([
             "",
             &config.name,
-            &get_test_file_path("hdfs-logs-multitenants-2.json").to_string_lossy(),
+            &get_test_file_path(index_test_file).to_string_lossy(),
+            "--build-dir",
+            &temp_path,
         ]),
         &postgres.pool,
     )
@@ -46,13 +81,7 @@ async fn test_example_config() -> Result<()> {
 
     let (tx, mut rx) = mpsc::channel(1);
     run_search_with_callback(
-        SearchArgs::parse_from([
-            "",
-            &config.name,
-            "tenant_id:>50 AND severity_text:INFO",
-            "--limit",
-            "1",
-        ]),
+        SearchArgs::parse_from(["", &config.name, query, "--limit", "1"]),
         &postgres.pool,
         Box::new(move |doc| {
             tx.try_send(doc).unwrap();
@@ -60,10 +89,7 @@ async fn test_example_config() -> Result<()> {
     )
     .await?;
 
-    assert_eq!(
-        rx.recv().await.unwrap(),
-        r#"{"attributes":{"class":"org.apache.hadoop.hdfs.server.datanode.DataNode"},"body":"PacketResponder: BP-108841162-10.10.34.11-1440074360971:blk_1074072698_331874, type=HAS_DOWNSTREAM_IN_PIPELINE terminating","resource":{"service":"datanode/01"},"severity_text":"INFO","tenant_id":58,"timestamp":"2016-04-13T06:46:53Z"}"#
-    );
+    assert_eq!(rx.recv().await.unwrap(), expected_output);
 
     run_drop(DropArgs::parse_from(["", &config.name]), &postgres.pool).await?;
 
