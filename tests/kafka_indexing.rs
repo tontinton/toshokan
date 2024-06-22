f6711767db602b86a0572a68c26181e5c10aed56
Tony Solomonik <tony.solomonik@gmail.com>
2024-06-23 00:11:55 +0300

    tests: kafka: Add checkpoint test

--- tests/kafka_indexing.rs
+++ tests/kafka_indexing.rs
@@ -3,9 +3,11 @@ mod common;
 use std::{
     path::{Path, PathBuf},
     str::FromStr,
+    sync::Arc,
     time::Duration,
 };
 
+use async_trait::async_trait;
 use clap::Parser;
 use color_eyre::{
     eyre::{bail, Context},
@@ -13,10 +15,7 @@ use color_eyre::{
 };
 use ctor::ctor;
 use log::debug;
-use notify::{
-    event::{CreateKind, RemoveKind},
-    recommended_watcher, Event, EventKind, RecursiveMode, Watcher,
-};
+use notify::{event::CreateKind, recommended_watcher, Event, EventKind, RecursiveMode, Watcher};
 use rdkafka::{
     producer::{FutureProducer, FutureRecord, Producer},
     ClientConfig,
@@ -27,7 +26,7 @@ use testcontainers_modules::kafka::{Kafka as KafkaContainer, KAFKA_PORT};
 use tokio::{
     fs::{create_dir_all, remove_dir_all},
     join, select, spawn,
-    sync::{mpsc, oneshot},
+    sync::{mpsc, oneshot, Mutex},
     task::JoinHandle,
 };
 use toshokan::{
@@ -35,7 +34,10 @@ use toshokan::{
     commands::{
         create::run_create_from_config,
         index::{run_index, BatchResult, IndexRunner},
-        sources::kafka_source::parse_url,
+        sources::{
+            kafka_source::{parse_url, KafkaSource},
+            Source, SourceItem,
+        },
     },
     config::IndexConfig,
 };
@@ -238,6 +240,84 @@ async fn test_kafka_index_stream_restart_on_rebalance() -> Result<()> {
         result = index_handle2 => {
             assert_eq!(result?, BatchResult::Restart);
         }
+    }
+
+    Ok(())
+}
+
+struct ArcSource(Arc<Mutex<dyn Source + Send>>);
+
+#[async_trait]
+impl Source for ArcSource {
+    async fn get_one(&mut self) -> Result<SourceItem> {
+        self.0.clone().lock_owned().await.get_one().await
+    }
+
+    async fn on_index_created(&mut self) -> Result<()> {
+        self.0.clone().lock_owned().await.on_index_created().await
+    }
+}
+
+#[tokio::test]
+async fn test_kafka_index_stream_load_checkpoint() -> Result<()> {
+    let postgres = run_postgres().await?;
+
+    let kafka_container = KafkaContainer::default().start().await?;
+    let kafka_port = kafka_container.get_host_port_ipv4(KAFKA_PORT).await?;
+    let kafka_url = format!("kafka://127.0.0.1:{kafka_port}/test_topic");
+
+    let mut config = IndexConfig::from_str(include_str!("../example_config.yaml"))?;
+    config.path = "/tmp/toshokan_kafka_stream_load_checkpoint".to_string();
+
+    // Just in case this path already exists, remove it.
+    let _ = remove_dir_all(&config.path).await;
+    create_dir_all(&config.path).await?;
+
+    run_create_from_config(&config, &postgres.pool).await?;
+
+    produce_logs(
+        &kafka_url,
+        include_str!("test_files/hdfs-logs-multitenants-2.json"),
+    )
+    .await?;
+
+    // Save checkpoint.
+    {
+        let kafka_source = Arc::new(Mutex::new(
+            KafkaSource::from_url(&kafka_url, true, &postgres.pool).await?,
+        ));
+
+        assert_eq!(
+            kafka_source.lock().await.loaded_partitions_and_offsets,
+            vec![(0, None)]
+        );
+
+        let mut runner = IndexRunner::new_with_source(
+            IndexArgs::parse_from([
+                "",
+                &config.name,
+                &kafka_url,
+                "--stream",
+                "--commit-interval",
+                "500ms",
+            ]),
+            postgres.pool.clone(),
+            Box::new(ArcSource(kafka_source.clone())),
+        )
+        .await?;
+
+        while kafka_source.lock().await.saved_partitions_and_offsets != vec![(0, 2)] {
+            assert_eq!(runner.run_one_batch().await?, BatchResult::Timeout);
+        }
+    }
+
+    // Load checkpoint.
+    {
+        let kafka_source = KafkaSource::from_url(&kafka_url, true, &postgres.pool).await?;
+        assert_eq!(
+            kafka_source.loaded_partitions_and_offsets,
+            vec![(0, Some(2))]
+        );
     }
 
     Ok(())
