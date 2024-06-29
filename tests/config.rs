cd897602ab3a128fe3d0684166db3c044e622e50
Tony Solomonik <tony.solomonik@gmail.com>
2024-06-29 17:40:05 +0300

    tests: config: Add s3 as storage tests
    Using the localstack testcontainers module.

--- tests/config.rs
+++ tests/config.rs
@@ -3,14 +3,23 @@ mod common;
 use std::{path::Path, str::FromStr};
 
 use async_tempfile::TempFile;
+use aws_sdk_s3::{
+    config::{Credentials, Region, SharedCredentialsProvider},
+    Client,
+};
 use clap::Parser;
 use color_eyre::Result;
 use ctor::ctor;
+use lazy_static::lazy_static;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use rstest::rstest;
+use rstest_reuse::{self, *};
+use testcontainers::{runners::AsyncRunner, ContainerAsync, RunnableImage};
+use testcontainers_modules::localstack::LocalStack;
 use tokio::{
     fs::{read_dir, remove_dir_all},
     io::AsyncWriteExt,
-    sync::mpsc,
+    sync::{mpsc, Mutex},
 };
 use toshokan::{
     args::{DropArgs, IndexArgs, SearchArgs},
@@ -23,9 +32,20 @@ use toshokan::{
 
 use crate::common::{run_postgres, test_init};
 
+lazy_static! {
+    static ref ENV_VAR_LOCK: Mutex<()> = Mutex::new(());
+}
+
 #[ctor]
 fn init() {
     test_init();
+}
+
+async fn run_localstack(services: &str) -> Result<ContainerAsync<LocalStack>> {
+    let image: RunnableImage<LocalStack> = LocalStack::default().into();
+    let image = image.with_env_var(("SERVICES", services));
+    let container = image.start().await?;
+    Ok(container)
 }
 
 async fn get_number_of_files_in_dir<P: AsRef<Path>>(dir: P) -> std::io::Result<usize> {
@@ -39,6 +59,7 @@ async fn get_number_of_files_in_dir<P: AsRef<Path>>(dir: P) -> std::io::Result<u
     Ok(file_count)
 }
 
+#[template]
 #[rstest]
 #[case::example_config(
     include_str!("../example_config.yaml"),
@@ -50,7 +71,7 @@ async fn get_number_of_files_in_dir<P: AsRef<Path>>(dir: P) -> std::io::Result<u
     "
 version: 1
 name: array_test
-path: /tmp/toshokan_array
+path: /tmp/toshokan
 schema:
   fields:
     - name: array
@@ -66,7 +87,7 @@ schema:
     "
 version: 1
 name: boolean_test
-path: /tmp/toshokan_boolean
+path: /tmp/toshokan
 schema:
   fields:
     - name: something
@@ -83,7 +104,7 @@ schema:
     "
 version: 1
 name: ip_test
-path: /tmp/toshokan_ip
+path: /tmp/toshokan
 schema:
   fields:
     - name: something
@@ -101,7 +122,7 @@ schema:
     "
 version: 1
 name: parse_string_test
-path: /tmp/toshokan_parse_string
+path: /tmp/toshokan
 schema:
   fields:
     - name: negative
@@ -129,25 +150,49 @@ schema:
 )]
 #[trace]
 #[tokio::test]
-async fn test_config(
+async fn bunch_of_configs(
+    #[case] raw_config: &str,
+    #[case] index_input: &str,
+    #[case] query: &str,
+    #[case] expected_output: &str,
+) {
+}
+
+#[apply(bunch_of_configs)]
+async fn test_config_fs(
     #[case] raw_config: &str,
     #[case] index_input: &str,
     #[case] query: &str,
     #[case] expected_output: &str,
 ) -> Result<()> {
     let postgres = run_postgres().await?;
-    let config = IndexConfig::from_str(raw_config)?;
+    let mut config = IndexConfig::from_str(raw_config)?;
+
+    config.path = format!(
+        "/tmp/{}",
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(16)
+            .map(char::from)
+            .collect::<String>()
+    );
 
     // Just in case this path already exists, remove it.
     let _ = remove_dir_all(&config.path).await;
 
     run_create_from_config(&config, &postgres.pool).await?;
 
-    let mut index_file = TempFile::new().await?;
-    index_file.write_all(index_input.trim().as_bytes()).await?;
+    let mut file_to_index = TempFile::new().await?;
+    file_to_index
+        .write_all(index_input.trim().as_bytes())
+        .await?;
 
     run_index(
-        IndexArgs::parse_from(["", &config.name, &index_file.file_path().to_string_lossy()]),
+        IndexArgs::parse_from([
+            "",
+            &config.name,
+            &file_to_index.file_path().to_string_lossy(),
+        ]),
         &postgres.pool,
     )
     .await?;
@@ -169,6 +214,74 @@ async fn test_config(
     run_drop(DropArgs::parse_from(["", &config.name]), &postgres.pool).await?;
 
     assert_eq!(get_number_of_files_in_dir(&config.path).await?, 0);
+
+    Ok(())
+}
+
+#[apply(bunch_of_configs)]
+async fn test_config_s3(
+    #[case] raw_config: &str,
+    #[case] index_input: &str,
+    #[case] query: &str,
+    #[case] expected_output: &str,
+) -> Result<()> {
+    let postgres = run_postgres().await?;
+    let mut config = IndexConfig::from_str(raw_config)?;
+
+    // Running in an isolated container, no need for random path.
+    config.path = "s3://toshokan".to_string();
+
+    let container = run_localstack("s3").await?;
+    let s3_port = container.get_host_port_ipv4(4566).await?;
+    let endpoint_url = format!("http://127.0.0.1:{s3_port}");
+
+    let client = Client::from_conf(
+        aws_sdk_s3::Config::builder()
+            .region(Region::new("us-east-1"))
+            .endpoint_url(&endpoint_url)
+            .credentials_provider(SharedCredentialsProvider::new(Credentials::new(
+                "test1", "test2", None, None, "local",
+            )))
+            .build(),
+    );
+    client.create_bucket().bucket("toshokan").send().await?;
+
+    run_create_from_config(&config, &postgres.pool).await?;
+
+    let mut file_to_index = TempFile::new().await?;
+    file_to_index
+        .write_all(index_input.trim().as_bytes())
+        .await?;
+
+    let _lock = ENV_VAR_LOCK.lock().await;
+    std::env::set_var("S3_ENDPOINT", &endpoint_url);
+    std::env::set_var("AWS_ACCESS_KEY_ID", "test1");
+    std::env::set_var("AWS_SECRET_ACCESS_KEY", "test2");
+    std::env::set_var("AWS_REGION", "us-east-1");
+
+    run_index(
+        IndexArgs::parse_from([
+            "",
+            &config.name,
+            &file_to_index.file_path().to_string_lossy(),
+        ]),
+        &postgres.pool,
+    )
+    .await?;
+
+    let (tx, mut rx) = mpsc::channel(1);
+    run_search_with_callback(
+        SearchArgs::parse_from(["", &config.name, query, "--limit", "1"]),
+        &postgres.pool,
+        Box::new(move |doc| {
+            tx.try_send(doc).unwrap();
+        }),
+    )
+    .await?;
+
+    assert_eq!(rx.recv().await.unwrap(), expected_output);
+
+    run_drop(DropArgs::parse_from(["", &config.name]), &postgres.pool).await?;
 
     Ok(())
 }
