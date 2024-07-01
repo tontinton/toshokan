02082822a703b911f55ae09ef0369c2e2b89dfe0
Tony Solomonik <tony.solomonik@gmail.com>
2024-07-01 23:38:04 +0300

    drop: Add support for s3 removal of index files

--- src/commands/drop.rs
+++ src/commands/drop.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
+use std::sync::Arc;
 
-use color_eyre::{eyre::eyre, Result};
+use color_eyre::Result;
+use futures::future::join_all;
 use sqlx::{query, query_as, PgPool};
-use tokio::fs::remove_file;
 
 use crate::args::DropArgs;
 
-use super::get_index_path;
+use super::{get_index_path, get_operator};
 
 pub async fn run_drop(args: DropArgs, pool: &PgPool) -> Result<()> {
     let base_path = get_index_path(&args.name, pool).await?;
@@ -18,20 +18,26 @@ pub async fn run_drop(args: DropArgs, pool: &PgPool) -> Result<()> {
             .await?;
     let file_names_len = file_names.len();
 
-    for (file_name,) in file_names {
-        let _ = remove_file(
-            Path::new(&base_path)
-                .join(file_name)
-                .to_str()
-                .ok_or_else(|| eyre!("failed to build index file path"))?,
-        )
-        .await;
-    }
-
     query("DELETE FROM indexes WHERE name=$1")
         .bind(&args.name)
         .execute(pool)
         .await?;
+
+    let op = Arc::new(get_operator(&base_path).await?);
+    join_all(
+        file_names
+            .into_iter()
+            .map(|(file_name,)| (file_name, op.clone()))
+            .map(|(file_name, op)| async move {
+                if let Err(e) = op.delete(&file_name).await {
+                    warn!(
+                        "Failed to delete index file '{file_name}': {e}.
+Don't worry, this just means the file is leaked, but will never be read from again."
+                    );
+                }
+            }),
+    )
+    .await;
 
     info!(
         "Dropped index: {} ({} number of index files)",
